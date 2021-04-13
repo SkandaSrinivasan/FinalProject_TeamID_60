939cc3d36a9155156dadedb24e0f415733c0ea32
kulbirsingh30 <singh.kul@northeastern.edu>
2021-04-12 23:52:52 -0400

    login page

--- FinalProject/src/main/java/com/finalproject/finalproject/Login.java
+++ FinalProject/src/main/java/com/finalproject/finalproject/Login.java
@@ -37,6 +37,7 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(0, 181, 204));
@@ -100,7 +101,8 @@ public class Login extends javax.swing.JFrame {
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(374, 0, 700, 550));
 
-        pack();
+        setSize(new java.awt.Dimension(1066, 545));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
