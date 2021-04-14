344686b270e74a88b06653e716a6142842f820ec
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2021-04-14 14:09:47 -0400

    Added basic Business package and d4bo

---
+++ FinalProject/src/main/java/Business/ConfigureASystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Skanda
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        return system;
+    }
+    
+}
