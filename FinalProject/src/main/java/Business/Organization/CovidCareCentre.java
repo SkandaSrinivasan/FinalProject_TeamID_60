c5306c463ea98ba411a7d441e4140e53e36ba026
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2021-04-14 16:16:21 -0400

    Restructured packages

--- FinalProject/src/main/java/Business/Organization/CovidCareCentre.java
+++ FinalProject/src/main/java/Business/Organization/CovidCareCentre.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Business.Hospital;
+package Business.Organization;
 
 /**
  *
  * @author kulbi
  */
-public class VaccinationControl {
+public class CovidCareCentre {
     
 }
